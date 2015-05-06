69101778a2c20bec05dd6fb26c477b0f07fb42f4
stevroid <gentleman_of_fine_taste@yahoo.com>
2015-05-06 12:22:55 -0700

    Update steering2.0.ino

--- steering2.0/steering2.0.ino
+++ steering2.0/steering2.0.ino
@@ -17,13 +17,11 @@ bool left_turn = false;
 bool right_turn = false;
 
 void setup() {
-  //attachInterrupt(0, brakeToNotKill, FALLING);
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Motor party!");
   pinMode(APMIN, INPUT);//
   //pinMode(VCC, INPUT);
   //pinMode(GND,INPUT);
-
   motor.setSpeed(200);
   motor.run(RELEASE);
 }
@@ -33,28 +31,27 @@ int steer;//variable to determine direction
 void loop()
 {
   Serial.print("Inside LOOP before digitalwrite");
-
   Serial.print("Inside LOOP AFTER digitalwrite");
-  if( 1550 < APMIN < 1850 && !left_turn )// turn left if  
+  if( 1550 < APMIN < 1850 && !left_turn )// turn left  
   {
     right_turn = false;
     left_turn = true;
     steer = 0;
   }
-  if ( 1350 < APMIN < 1550)//go straight
+  if ( 1350 < APMIN < 1550)//go straight 
   {
-    if(left_turn)
+    if(left_turn)//if was previously left,turn right
     {
       steer = 1;
       left_turn = false;
     }
-    if(right_turn)
+    if(right_turn)//if was previously right, turn left
     {
       steer = 0;
       right_turn = false;
     }
   }
-  if( 1550 < APMIN < 1850 && !right_turn )
+  if( 1000 < APMIN < 1350 && !right_turn )
   {
     left_turn = false;
     right_turn = true;
@@ -73,7 +70,6 @@ void loop()
   }
   steer = 2;//used to hold position
 }
-
 void retract()// left turn
 {
   motor.run(BACKWARD);
@@ -84,7 +80,6 @@ void retract()// left turn
   motor.run(RELEASE);
   Serial.print("AFTER RELEASE RETRACT\n");
 }
-
 void extend()//right turn
 {
   // Serial.print("Forward function");
