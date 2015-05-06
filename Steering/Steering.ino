78afdd7209ed85c5291ad21316f7c0a24e3cc051
stevroid <gentleman_of_fine_taste@yahoo.com>
2015-05-06 12:14:13 -0700

    Update Steering.ino

--- Steering/Steering.ino
+++ Steering/Steering.ino
@@ -15,17 +15,13 @@ int APMIN = 13;
 //int GND = 11;
 bool left_turn = false;
 bool right_turn = false;
-bool straight = false;
 
 void setup() {
-  //attachInterrupt(0, brakeToNotKill, FALLING);
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Motor party!");
-  //pinMode(FLAG, INPUT); // turn on motor #2
   pinMode(APMIN, INPUT);//
   //pinMode(VCC, INPUT);
   //pinMode(GND,INPUT);
-
   motor.setSpeed(200);
   motor.run(RELEASE);
 }
@@ -36,43 +32,30 @@ void loop()
   Serial.print("Inside LOOP before digitalwrite");
 
   Serial.print("Inside LOOP AFTER digitalwrite");
-  if ( 1550 < APMIN < 1850  ) //left turning
+  if ( 1550 < APMIN < 1850 && !left_turn  ) //left turning
   {
     right_turn = false;
-    straight = false;
-    if (!left_turn)
-    {
-      left_turn = true;
-      //right_turn = false;
-      retract();
-    }
+    left_turn = true;
+    retract();
   }
   if (1350 < APMIN < 1550) // straight
   {
-    if (!straight)
+    if (left_turn)
     {
-      if (left_turn)
-      {
-        left_turn = false;
-        straight = true;
-        extend();
-      }
-      if (right_turn)
-      {
-        right_turn = false;
-        straight =  true;
-        retract();
-      }
-    }
-  }
-  if (1000 < APMIN < 1350) //right turning
-  {
-    left_turn = false;
-    if (!right_turn)
-    {
-      right_turn = true;
+      left_turn = false;
       extend();
     }
+    if (right_turn)
+    {
+      right_turn = false;
+      retract();
+    }
+  }
+  if (1000 < APMIN < 1350 && !right_turn) //right turning
+  {
+    left_turn = false;
+    right_turn = true;
+    extend();
   }
 }
 
