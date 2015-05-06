152391c20fd587af90b0f73bf3ca471baf2e373f
jbanh <jbanh12@gmail.com>
2015-05-06 11:39:10 -0700

    Update Gas.ino
    Added APM values to start acceleration or brake when it reaches a stopping point

--- Gas/Gas.ino
+++ Gas/Gas.ino
@@ -4,6 +4,9 @@
 // DC motor on M2
 // m1a m1b ground m2a = black wire m2b = red wire
 AF_DCMotor motor(2);
+// Adding APMIN
+int APMIN = 13;
+
 
 volatile bool wasStopped = true;
 volatile bool fullyExtended = false;
@@ -12,6 +15,7 @@ void setup() {
   attachInterrupt(0, brake, FALLING);
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Motor party!");
+  pinMode(APMIN, INPUT)
   motor.setSpeed(200);
   motor.run(RELEASE);
 }
@@ -20,14 +24,26 @@ int i;
 int j;
 void loop()
 {
-  if (wasStopped && !fullyExtended)
-  {
-    Serial.print("Inside extend");
-    accelerate();
-    wasStopped = false;
-    fullyExtended = true;
-  }
-
+	if ( 1550 < APMIN < 1900  ) //Going forward
+	{
+		if (wasStopped && !fullyExtended)
+		{
+			Serial.print("Inside extend");
+			accelerate();
+			wasStopped = false;
+			fullyExtended = true;
+		}
+	}
+	if ( 1350 < APMIN < 1550  ) //Stopping
+	{
+		if (!wasStopped && fullyExtended)
+		{
+			Serial.print("Retracting");
+			brake();
+			wasStopped = true;
+			fullyExtended = false;
+		}
+	}
 }
 
 void brake()
@@ -37,7 +53,7 @@ void brake()
     motor.run(BACKWARD);
     Serial.print("AFTER BACKWARD IN BRAKE\n");
     motor.setSpeed(255);
-    for (i = 0; i < 300; i++)
+    for (i = 0; i < 62; i++)
     {
       delayMicroseconds(16000);
     }
@@ -56,7 +72,7 @@ void accelerate()
   motor.run(FORWARD);
   Serial.print("AFTER FORWARD IN ACCELERATE\n");
   motor.setSpeed(255);
-  delay(6000);
+  delay(1000);
   Serial.print("BEFORE RELEASE IN ACCELERATE\n");
   motor.run(RELEASE);
   Serial.print("AFTER RELEASE IN ACCELERATE\n");
